fc82a2df5255299ff344fbeca1acab9e9fcf7079
Timur-jpg <timur.turdaliev08@gmail.com>
2025-09-05 20:10:57 -0400

    Answered until the 29 question Added getter method

--- TicketMachine.java
+++ TicketMachine.java
@@ -18,6 +18,11 @@ public class TicketMachine
     // The total amount of money collected by this machine.
     private int total;
     
+    public int getTotal()
+    {
+        return price;
+    }    
+    
     private int status;
 
     /**
